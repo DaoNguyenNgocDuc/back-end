5e978924f674d2157fafb28c12d29e0ff8290e14
DaoNguyenNgocDuc <daoduc132@gmail.com>
2023-03-17 06:00:50 +0700

    add new feature

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-//just a commentđâsdsdddd
+//just a comment
 const server = app.listen(process.env.PORT, ()=>{
     console.log(`Server started on PORT ${process.env.PORT}`);
 })
