1b530b4e3384279500ee5b8542720647ca16ec63
DaoNguyenNgocDuc <daoduc132@gmail.com>
2023-03-16 13:08:40 +0700

    add change in index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-//just a commentđâsd
+//just a commentđâsdsdddd
 const server = app.listen(process.env.PORT, ()=>{
     console.log(`Server started on PORT ${process.env.PORT}`);
 })
