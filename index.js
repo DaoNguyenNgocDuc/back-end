a02edb0d0564ecd47a24a943a42fbef53bd22c03
DaoNguyenNgocDuc <daoduc132@gmail.com>
2023-03-27 11:22:54 +0700

    update socket

--- index.js
+++ index.js
@@ -41,4 +41,12 @@ io.on("connection", (socket)=>{
     socket.on("add-user", (userId) => {
         onlineUsers.set(userId, socket.id);
     })
+
+    socket.on("send-msg", (data)=> {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    })
 })
+
