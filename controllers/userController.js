bb8c183caccb23bade891ebfaa08db86cbb13e05
DaoNguyenNgocDuc <daoduc132@gmail.com>
2023-04-07 22:37:04 +0700

    add unit test

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const { check } = require('express-validator');
 
 module.exports.register = async (req, res, next) => {
     try {
         const {username, email, password} = req.body;
         const usernameCheck = await User.findOne({username});
         if(usernameCheck)
-            return res.json({msg:"Username alread used", status: false});
+            return res.json({msg:"Username already used", status: false});
         const emailCheck = await User.findOne({email});
         if(emailCheck)
             return res.json({msg:"Email already used", status:false});
@@ -25,13 +26,14 @@ module.exports.register = async (req, res, next) => {
 
 module.exports.login = async (req, res, next) => {
     try {
-        const {username, password} = req.body;
-        const user = await User.findOne({username});
+        const {email, password} = req.body;
+        check({email}).isEmail().withMessage("Incorrect email format");
+        const user = await User.findOne({email});
         if(!user)
-            return res.json({msg:"Incorrect username or password", status: false});
+            return res.json({msg:"Incorrect email or password", status: false});
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if(!isPasswordValid)
-            return res.json({msg:"Incorrect username or password", status: false});
+            return res.json({msg:"Incorrect email or password", status: false});
         delete user.password;
         return res.json({status:true, user});
     } catch(ex) {
